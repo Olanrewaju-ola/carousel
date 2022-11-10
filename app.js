52dc1db7232dd562307a9891224878b033b6d701
Olanrewaju <114247644+Olanrewaju-ola@users.noreply.github.com>
2022-11-09 16:32:36 -0800

    files

---
+++ app.js
@@ -0,0 +1,43 @@
+let slidePosition = 0;
+const slides = document.getElementsByClassName('carousel_item');
+const totalSlides = slides.length;
+
+document.
+  getElementById('carousel_button_next')
+  .addEventListener("click", function() {
+    moveToNextSlide();
+  });
+document.
+  getElementById('carousel_button_prev')
+  .addEventListener("click", function() {
+    moveToPrevSlide();
+  });
+
+function updateSlidePosition() {
+  for (let slide of slides) {
+    slide.classList.remove('carousel_item_visible');
+    slide.classList.add('carousel_item_hidden');
+  }
+
+  slides[slidePosition].classList.add('carousel_item_visible');
+}
+
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+
+  updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+
+  updateSlidePosition();
+}
